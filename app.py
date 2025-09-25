416f15f8318593d3ab3d4b9a67ae5fb41eb69712
kalyanigrandhi <kalyanigrandhi2006@gmail.com>
2025-09-25 17:31:28 +0530

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     # Display image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Dummy crowd count (replace with CSRNet later)
     st.success(f"Estimated Crowd Count: {np.random.randint(50,500)}")
